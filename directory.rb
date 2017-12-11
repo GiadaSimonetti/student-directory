14ddc47b522f9318cbd849a5ea07335248f2b781
giada <giada.simonetti@gmail.com>
2017-12-11 15:43:03 +0000

    Add user input

--- directory.rb
+++ directory.rb
@@ -1,17 +1,38 @@
 #let's put all students into an array
-students = [
-  {name: "Dr Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex Delarge", cohort: :november},
-  {name: "The Wicked Witch of West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+# students = [
+#   {name: "Dr Hannibal Lecter", cohort: :november},
+#   {name: "Darth Vader", cohort: :november},
+#   {name: "Nurse Ratched", cohort: :november},
+#   {name: "Michael Corleone", cohort: :november},
+#   {name: "Alex Delarge", cohort: :november},
+#   {name: "The Wicked Witch of West", cohort: :november},
+#   {name: "Terminator", cohort: :november},
+#   {name: "Freddy Krueger", cohort: :november},
+#   {name: "The Joker", cohort: :november},
+#   {name: "Joffrey Baratheon", cohort: :november},
+#   {name: "Norman Bates", cohort: :november}
+# ]
+
+
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit enter twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty repeate this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -29,6 +50,7 @@ def print_footer(students)
 end
 
 # nothing appens untill we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
