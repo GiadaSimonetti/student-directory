28a0a9d8c2c260739a3f2c9bb15aa8d045295cf5
giada <giada.simonetti@gmail.com>
2017-12-13 11:48:56 +0000

    Change 'chomp' with 'strip' - Exercise 10

--- directory.rb
+++ directory.rb
@@ -1,36 +1,17 @@
-#let's put all students into an array
-# students = [
-#   {name: "Dr Hannibal Lecter", cohort: :november},
-#   {name: "Darth Vader", cohort: :november},
-#   {name: "Nurse Ratched", cohort: :november},
-#   {name: "Michael Corleone", cohort: :november},
-#   {name: "Alex Delarge", cohort: :november},
-#   {name: "The Wicked Witch of West", cohort: :november},
-#   {name: "Terminator", cohort: :november},
-#   {name: "Freddy Krueger", cohort: :november},
-#   {name: "The Joker", cohort: :november},
-#   {name: "Joffrey Baratheon", cohort: :november},
-#   {name: "Norman Bates", cohort: :november}
-# ]
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit enter twice"
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.strip
   # while the name is not empty repeate this code
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
-    if students.count == 1
-      puts "Now we have #{students.count} student"
-    else
-      puts "Now we have #{students.count} students"
-    end
+    puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = gets.strip
   end
   # return the array of students
   students
